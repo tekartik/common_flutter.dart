40c3c9957105d180bebf320de9be71afc2d5a14e
Alexandre Roux <alex@tekartik.com>
2025-06-27 14:58:29 +0200

    dart 3.8 support

--- packages/lints_flutter/tool/list_all_rules.dart
+++ packages/lints_flutter/tool/list_all_rules.dart
@@ -4,11 +4,10 @@ import 'package:path/path.dart';
 import 'package:process_run/shell.dart';
 
 Future<void> main() async {
-  var files =
-      await Directory('lib')
-          .list()
-          .where((fse) => fse is File && extension(fse.path) == '.yaml')
-          .toList();
+  var files = await Directory('lib')
+      .list()
+      .where((fse) => fse is File && extension(fse.path) == '.yaml')
+      .toList();
   for (var file in files) {
     stdout.writeln('# ${file.path}');
     var content = (await run('tklint list-rules ${file.path}')).first.outText;
